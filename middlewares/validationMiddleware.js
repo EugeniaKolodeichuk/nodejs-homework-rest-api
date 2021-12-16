b9a6a56c86ccd594b33ecdef4b76cbb7466f82df
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2021-12-16 15:31:57 +0200

    add hw

---
+++ middlewares/validationMiddleware.js
@@ -0,0 +1,54 @@
+const Joi = require("joi");
+
+module.exports = {
+  addContactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().min(3).max(30).required(),
+      email: Joi.string().min(3).max(30).required(),
+      phone: Joi.number().min(3).required(),
+    });
+
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+      return res.status(400).json({
+        message: `Missing required name field`,
+        status: validationResult.error.details,
+      });
+    }
+
+    next();
+  },
+
+  putContactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().min(3).max(30).required(),
+      email: Joi.string().min(3).max(30).required(),
+      phone: Joi.number().min(3).required(),
+    });
+
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+      return res.status(400).json({
+        message: `Missing fields`,
+        status: validationResult.error.details,
+      });
+    }
+    next();
+  },
+
+  patchContactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().min(3).max(30).optional(),
+      email: Joi.string().min(3).max(30).optional(),
+      phone: Joi.number().min(3).optional(),
+    });
+
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+      return res.status(400).json({
+        status: validationResult.error.details,
+      });
+    }
+    next();
+  },
+};
